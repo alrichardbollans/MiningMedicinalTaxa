b0a8691edc631564221aa4910b1db63fb51343ee
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-07-19 18:38:45 +0100

    begin methods for downloading full dataset, reading and filtering potential useful cases.

--- literature_downloads/semantic_scholar/download_data.py
+++ literature_downloads/semantic_scholar/download_data.py
@@ -2,84 +2,127 @@
 # which are much smaller than the full datasets.
 import json
 import os.path
+from typing import List
 
-from literature_downloads.semantic_scholar import download_path
+import pandas as pd
+
+from literature_downloads.filter_terms import search_filtering_terms
+from literature_downloads.semantic_scholar import download_path, S2_API_KEY
 
 dataset_download_path = os.path.join(download_path, 'datasets')
+abstracts_path = os.path.join(download_path, 'abstracts')
+text_path = os.path.join(download_path, 'text')
+paper_info_path = os.path.join(download_path, 'paper_info')
 if not os.path.exists(dataset_download_path):
     os.mkdir(dataset_download_path)
 
 
-def convert_corpus_id_to_paper_id(corpus_id) -> str:
-    docs = [json.loads(l) for l in open("samples/paper-ids/paper-ids-sample.jsonl", "r").readlines()]
-    shas = [x['sha'] for x in docs if x['corpusid'] == corpus_id]
-    return shas
+def get_zip_file_for_part(part: int):
+    return os.path.join(dataset_download_path, "s2orc-part" + str(part) + ".jsonl.gz")
 
 
-def look_at_sample():
-    # subprocess.check_call("bash get_sample_files.sh", shell=True)
-
-    # want to use paper id to get OCR data
-    # use paper-ids to map between corpus ids and paperids
-
-    # S2ORC
-    docs = [json.loads(l) for l in open("samples/s2orc/s2orc-sample.jsonl", "r").readlines()]
-    for paper in docs:
-        text = paper['content']['text']
-        annotations = {k: json.loads(v) for k, v in paper['content']['annotations'].items() if v}
-
-        for a in annotations['paragraph'][:10]:
-            print(a)
-        for a in annotations['bibref'][:10]:
-            print(a)
-        for a in annotations['bibentry'][:10]:
-            print(a)
-
-        def text_of(type):
-            return [text[a['start']:a['end']] for a in annotations[type]]
-
-        paragraph_text = text_of('paragraph')
-        paper_id = convert_corpus_id_to_paper_id(paper['corpusid'])
-        print('\n\n'.join(text_of('paragraph')[:3]))
-        print()
+def get_unzipped_file_for_part(part: int):
+    return os.path.join(dataset_download_path, "s2orc-part" + str(part) + ".jsonl")
 
 
 def download_fullset():
     import requests
     import urllib
-    import os
+    for part in range(1, 30):
+        # Get info about the latest release
+        latest_release = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest").json()
+        print(latest_release['README'])
+        print(latest_release['release_id'])
 
-    # Get info about the latest release
-    latest_release = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest").json()
-    print(latest_release['README'])
-    print(latest_release['release_id'])
+        # Print names of datasets in the release
+        print("\n".join(d['name'] for d in latest_release['datasets']))
 
-    # Get info about past releases
-    dataset_ids = requests.get("http://api.semanticscholar.org/datasets/v1/release").json()
+        # Print README for one of the datasets
+        print(latest_release['datasets'][2]['README'])
 
-    # Print names of datasets in the release
-    print("\n".join(d['name'] for d in latest_release['datasets']))
+        # Get info about the s2orc dataset
+        s2orc = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest/dataset/s2orc",
+                             headers={'x-api-key': S2_API_KEY}).json()
 
-    # Print README for one of the datasets
-    print(latest_release['datasets'][2]['README'])
+        # Download the first part of the dataset
+        urllib.request.urlretrieve(s2orc['files'][part],
+                                   get_zip_file_for_part(part))
 
-    # Get info about the papers dataset
-    s2orc = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest/dataset/s2orc",
-                         headers={'x-api-key': os.getenv("S2_API_KEY")}).json()
+    # extract to file
+    import gzip
+    import shutil
 
-    # Download the first part of the dataset
-    urllib.request.urlretrieve(s2orc['files'][0],
-                               os.path.join(dataset_download_path, "s2orc-part0.jsonl.gz"))
+    for i in [0]:  # range(0, 30):
+        if not os.path.exists(get_unzipped_file_for_part(i)):
+            zipfile = get_zip_file_for_part(i)
+            with gzip.open(zipfile, 'rb') as f_in:
+                with open(get_unzipped_file_for_part(i), 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
 
-    # Get info about the papers dataset
-    paper_ids = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest/dataset/paper-ids",
-                             headers={'x-api-key': os.getenv("S2_API_KEY")}).json()
 
-    # Download the first part of the dataset
-    urllib.request.urlretrieve(paper_ids['files'][0],
-                               os.path.join(dataset_download_path, "paper-ids-part0.jsonl.gz"))
+def get_relevant_papers_from_download(query: List[str]):
+    paper_df = pd.DataFrame()
+    relevant_abstracts = {}
+    relevant_text = {}
+
+    # currently just for data 0
+    with open(get_unzipped_file_for_part(0), 'r') as infile:
+        for line in infile:
+            paper = json.loads(line)
+            text = paper['content']['text']
+            annotations = {k: json.loads(v) for k, v in paper['content']['annotations'].items() if v}
+
+            def text_of(type):
+                types = annotations.get(type, '')
+                return [text[int(a['start']):int(a['end'])] for a in types]
+
+            if text is not None:
+                if any(x in text for x in query):
+                    corpusid = str(paper['corpusid'])
+                    abstract = ' '.join(set(text_of('abstract')))
+                    title = ' '.join(set(text_of('title')))
+                    authors = ', '.join(set(text_of('author')))
+                    try:
+                        url = paper['content']['source']['oainfo']['openaccessurl']
+                    except TypeError:
+                        url = None
+
+                    relevant_abstracts[corpusid] = abstract
+                    relevant_text[corpusid] = text
+                    try:
+                        doi = paper['externalids']['doi']
+                    except TypeError:
+                        doi = None
+
+                    info_df = pd.DataFrame(
+                        {'corpusid': [corpusid], 'DOI': [doi],
+                         'title': [title], 'authors': [authors], 'oaurl': [url],
+                         'abstract_path': [os.path.join(abstracts_path, corpusid + '.txt')],
+                         'text_path': [os.path.join(text_path, corpusid + '.txt')]})
+                    paper_df = pd.concat([paper_df, info_df])
+
+    for c_id in relevant_abstracts:
+        abstract = relevant_abstracts[c_id]
+        if abstract is not None:
+            f = open(os.path.join(abstracts_path, c_id + '.txt'), 'w')
+            f.write(abstract)
+            f.close()
+
+    for c_id in relevant_text:
+        te = relevant_text[c_id]
+        if te is not None:
+            f = open(os.path.join(text_path, c_id + '.txt'), 'w')
+            f.write(te)
+            f.close()
+
+    paper_df.to_csv(os.path.join(paper_info_path, '_'.join(query) + '.csv'))
 
 
 if __name__ == '__main__':
-    # look_at_sample()
-    download_fullset()
+    # # look_at_sample()
+    # get_relevant_papers_from_download("samples/s2orc/s2orc-sample.jsonl")
+    # Download then check they're all there
+    # TODO: find number of datasets. Slightly confused by this
+    # download_fullset()
+
+    get_relevant_papers_from_download(query=search_filtering_terms)
