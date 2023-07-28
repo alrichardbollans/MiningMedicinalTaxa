b3c2a9fb2a9271122b0813cefd88e44feac25d9e
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-07-28 15:07:03 +0100

    Begin core download and finalise sem scholar download

---
+++ literature_downloads/core/download_data.py
@@ -0,0 +1,94 @@
+import json
+import os
+
+import dotenv
+from typing import List
+
+import pandas as pd
+from tqdm import tqdm
+
+from literature_downloads.filter_terms import is_relevant_text, query_name
+
+dotenv.load_dotenv()
+CORE_API_KEY = os.environ['CORE_API_KEY']
+headers = {
+    'x-api-key': CORE_API_KEY,
+}
+
+core_download_path = os.path.join('downloads')
+core_dataset_download_path = os.path.join(core_download_path, 'datasets')
+core_abstracts_path = os.path.join(core_download_path, 'abstracts')
+core_text_path = os.path.join(core_download_path, 'text')
+core_paper_info_path = os.path.join(core_download_path, 'paper_info')
+for p in [core_download_path, core_dataset_download_path, core_text_path, core_paper_info_path, core_abstracts_path]:
+    if not os.path.exists(p):
+        os.mkdir(p)
+
+
+def download_full_core_dataset():
+    # dataset docs = https://core.ac.uk/documentation/dataset
+    import wget
+
+    wget.download('https://core.ac.uk/datasets/core_2020-12-20_resync.tar.xz', out=core_dataset_download_path)
+
+
+def get_relevant_papers_from_download():
+    paper_df = pd.DataFrame()
+    relevant_abstracts = {}
+    relevant_text = {}
+
+    import tarfile
+
+    zipfile = os.path.join(core_dataset_download_path, 'core_2020-12-20_resync.tar.xz')
+    print('unzipping')
+    # TODO: Add thread pool
+    with tarfile.open(zipfile, 'r') as main_archive:
+
+        for member in main_archive:
+            print(member)
+            print(len(relevant_text))
+            file_obj = main_archive.extractfile(member)
+            with tarfile.open(fileobj=file_obj, mode='r') as sub_archive:
+                members = sub_archive.getmembers()
+                for i in tqdm(range(len(members))):
+                    m = members[i]
+                    if m.name.endswith('.json'):
+                        f = sub_archive.extractfile(m)
+                        lines = f.readlines()
+                        paper = json.loads(lines[0])
+                        text = paper['fullText']
+                        if is_relevant_text(text):
+                            corpusid = paper['coreId']
+
+                            relevant_abstracts[corpusid] = paper['abstract']
+                            relevant_text[corpusid] = text
+
+                            info_df = pd.DataFrame(
+                                {'corpusid': [corpusid], 'DOI': [paper['doi']],
+                                 'title': [paper['title']], 'authors': [str(paper['authors'])], 'oaurl': [paper['downloadUrl']],
+                                 'abstract_path': [os.path.join(core_abstracts_path, corpusid + '.txt')],
+                                 'text_path': [os.path.join(core_text_path, corpusid + '.txt')]})
+                            paper_df = pd.concat([paper_df, info_df])
+    for c_id in relevant_abstracts:
+        abstract = relevant_abstracts[c_id]
+        if abstract is not None:
+            f = open(os.path.join(core_abstracts_path, c_id + '.txt'), 'w')
+            f.write(abstract)
+            f.close()
+
+    for c_id in relevant_text:
+        te = relevant_text[c_id]
+        if te is not None:
+            f = open(os.path.join(core_text_path, c_id + '.txt'), 'w')
+            f.write(te)
+            f.close()
+
+    paper_df.to_csv(os.path.join(core_paper_info_path, query_name + '.csv'))
+    return paper_df
+
+
+if __name__ == '__main__':
+    # Getting an EOFError: Compressed file ended before the end-of-stream marker was reached
+    # ./0f1/3b/223015690.json is not complete
+    # wget -c https://core.ac.uk/datasets/core_2020-12-20_resync.tar.xz says file is fullly retrieved...
+    get_relevant_papers_from_download()
--- literature_downloads/semantic_scholar/download_data.py
+++ literature_downloads/semantic_scholar/download_data.py
@@ -1,86 +1,102 @@
-# Shows how to download and inspect data in the sample datasets
-# which are much smaller than the full datasets.
 import json
 import os.path
+import time
 from typing import List
 
 import pandas as pd
 
-from literature_downloads.filter_terms import search_filtering_terms
-from literature_downloads.semantic_scholar import download_path, S2_API_KEY
+import dotenv
 
-dataset_download_path = os.path.join(download_path, 'datasets')
-abstracts_path = os.path.join(download_path, 'abstracts')
-text_path = os.path.join(download_path, 'text')
-paper_info_path = os.path.join(download_path, 'paper_info')
-if not os.path.exists(dataset_download_path):
-    os.mkdir(dataset_download_path)
+from literature_downloads.filter_terms import is_relevant_text, query_name
+
+dotenv.load_dotenv()
+S2_API_KEY = os.environ['S2_API_KEY']
+headers = {
+    'x-api-key': S2_API_KEY,
+}
+
+sem_schol_download_path = os.path.join('downloads')
+sem_schol_dataset_download_path = os.path.join(sem_schol_download_path, 'datasets')
+sem_schol_abstracts_path = os.path.join(sem_schol_download_path, 'abstracts')
+sem_schol_text_path = os.path.join(sem_schol_download_path, 'text')
+sem_schol_paper_info_path = os.path.join(sem_schol_download_path, 'paper_info')
+if not os.path.exists(sem_schol_dataset_download_path):
+    os.mkdir(sem_schol_dataset_download_path)
 
 
 def get_zip_file_for_part(part: int):
-    return os.path.join(dataset_download_path, "s2orc-part" + str(part) + ".jsonl.gz")
+    return os.path.join(sem_schol_dataset_download_path, "s2orc-part" + str(part) + ".jsonl.gz")
 
 
 def get_unzipped_file_for_part(part: int):
-    return os.path.join(dataset_download_path, "s2orc-part" + str(part) + ".jsonl")
+    return os.path.join(sem_schol_dataset_download_path, "s2orc-part" + str(part) + ".jsonl")
 
 
 def download_fullset():
     import requests
     import urllib
-    for part in range(1, 30):
-        # Get info about the latest release
-        latest_release = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest").json()
-        print(latest_release['README'])
-        print(latest_release['release_id'])
+    # Get info about the latest release
+    latest_release = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest").json()
+    print(latest_release['README'])
+    print(latest_release['release_id'])
 
-        # Print names of datasets in the release
-        print("\n".join(d['name'] for d in latest_release['datasets']))
+    # Print names of datasets in the release
+    print("\n".join(d['name'] for d in latest_release['datasets']))
 
-        # Print README for one of the datasets
-        print(latest_release['datasets'][2]['README'])
+    # Print README for one of the datasets
+    print(latest_release['datasets'][2]['README'])
 
+    for part in range(0, 30):
         # Get info about the s2orc dataset
         s2orc = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest/dataset/s2orc",
                              headers={'x-api-key': S2_API_KEY}).json()
-
-        # Download the first part of the dataset
+        print(part)
+        # Download the part of the dataset
         urllib.request.urlretrieve(s2orc['files'][part],
                                    get_zip_file_for_part(part))
+        print(f'part {part} done')
+        time.sleep(5)
 
     # extract to file
-    import gzip
-    import shutil
+    # import gzip
+    # import shutil
+    #
+    # for i in [0]:  # range(0, 30):
+    #     if not os.path.exists(get_unzipped_file_for_part(i)):
+    #         zipfile = get_zip_file_for_part(i)
+    #         with gzip.open(zipfile, 'rb') as f_in:
+    #             with open(get_unzipped_file_for_part(i), 'wb') as f_out:
+    #                 shutil.copyfileobj(f_in, f_out)
 
-    for i in [0]:  # range(0, 30):
-        if not os.path.exists(get_unzipped_file_for_part(i)):
-            zipfile = get_zip_file_for_part(i)
-            with gzip.open(zipfile, 'rb') as f_in:
-                with open(get_unzipped_file_for_part(i), 'wb') as f_out:
-                    shutil.copyfileobj(f_in, f_out)
 
-
-def get_relevant_papers_from_download(query: List[str]):
+def get_relevant_papers_from_download():
     paper_df = pd.DataFrame()
     relevant_abstracts = {}
     relevant_text = {}
 
-    # currently just for data 0
-    with open(get_unzipped_file_for_part(0), 'r') as infile:
-        for line in infile:
-            paper = json.loads(line)
-            text = paper['content']['text']
-            annotations = {k: json.loads(v) for k, v in paper['content']['annotations'].items() if v}
+    import gzip
+    from tqdm import tqdm
+    # TODO: Add thread pool
+    for part in tqdm(range(0, 30)):
+        zipfile = get_zip_file_for_part(part)
+        print('unzipping')
+        with gzip.open(zipfile, 'rb') as infile:
+            print('unzipped')
+            for line in infile:
+                paper = json.loads(line)
+                text = paper['content']['text']
+                annotations = {k: json.loads(v) for k, v in paper['content']['annotations'].items() if v}
 
-            def text_of(type):
-                types = annotations.get(type, '')
-                return [text[int(a['start']):int(a['end'])] for a in types]
+                def text_of(type):
+                    types = annotations.get(type, '')
+                    return [text[int(a['start']):int(a['end'])] for a in types]
 
-            if text is not None:
-                if any(x in text for x in query):
+                if is_relevant_text(text):
+
                     corpusid = str(paper['corpusid'])
                     abstract = ' '.join(set(text_of('abstract')))
                     title = ' '.join(set(text_of('title')))
+                    print(title)
                     authors = ', '.join(set(text_of('author')))
                     try:
                         url = paper['content']['source']['oainfo']['openaccessurl']
@@ -97,32 +113,42 @@ def get_relevant_papers_from_download(query: List[str]):
                     info_df = pd.DataFrame(
                         {'corpusid': [corpusid], 'DOI': [doi],
                          'title': [title], 'authors': [authors], 'oaurl': [url],
-                         'abstract_path': [os.path.join(abstracts_path, corpusid + '.txt')],
-                         'text_path': [os.path.join(text_path, corpusid + '.txt')]})
+                         'abstract_path': [os.path.join(sem_schol_abstracts_path, corpusid + '.txt')],
+                         'text_path': [os.path.join(sem_schol_text_path, corpusid + '.txt')]})
                     paper_df = pd.concat([paper_df, info_df])
 
     for c_id in relevant_abstracts:
         abstract = relevant_abstracts[c_id]
         if abstract is not None:
-            f = open(os.path.join(abstracts_path, c_id + '.txt'), 'w')
+            f = open(os.path.join(sem_schol_abstracts_path, c_id + '.txt'), 'w')
             f.write(abstract)
             f.close()
 
     for c_id in relevant_text:
         te = relevant_text[c_id]
         if te is not None:
-            f = open(os.path.join(text_path, c_id + '.txt'), 'w')
+            f = open(os.path.join(sem_schol_text_path, c_id + '.txt'), 'w')
             f.write(te)
             f.close()
 
-    paper_df.to_csv(os.path.join(paper_info_path, '_'.join(query) + '.csv'))
+    paper_df.to_csv(os.path.join(sem_schol_paper_info_path, query_name + '.csv'))
+    return paper_df
+
+
+def check_for_repetitions():
+    'corpusid'
+    paper_df = pd.read_csv(os.path.join(sem_schol_paper_info_path, query_name + '.csv'))
+    problems = paper_df[paper_df.duplicated(subset=['corpusid'])]
+
+    assert len(problems.index) == 0
+
+
+def check_for_subsumption_by_core():
+    pass
 
 
 if __name__ == '__main__':
-    # # look_at_sample()
-    # get_relevant_papers_from_download("samples/s2orc/s2orc-sample.jsonl")
     # Download then check they're all there
-    # TODO: find number of datasets. Slightly confused by this
     # download_fullset()
-
-    get_relevant_papers_from_download(query=search_filtering_terms)
+    get_relevant_papers_from_download()
+    check_for_repetitions()
