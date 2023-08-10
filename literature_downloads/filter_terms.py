bb2571674fa03be63d494a39973050fe550ad9c7
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-08-10 16:25:56 +0100

    update terms

--- literature_downloads/filter_terms.py
+++ literature_downloads/filter_terms.py
@@ -1,23 +1,122 @@
 import string
 
-# species_names = ['pubescens']
-# genus_names = ['Cinchona']
-# family_names = ['Rubiaceae']
+from typing import List
+from wcvp_download import get_all_taxa, wcvp_columns
+
+all_taxa = get_all_taxa()
+genus_names = all_taxa[wcvp_columns['genus']].unique().tolist()
+family_names = all_taxa[wcvp_columns['family']].unique().tolist()
 # common_names = []
+# species_names = ['pubescens']
 
-misc_keywords = ['medicinal', 'medicine', 'herbal', 'remedy', 'drug']  # add other languages
-misc_paired_keywords = []
-# plant_terms = species_names + genus_names + family_names
 
-query_name = '_'.join(misc_keywords + misc_paired_keywords)
+_en_keywords = ['medicinal',
+                'medicines',
+                'medicine',
+                'medics',
+                'medic',
+                'pharmacopoeia',
+                'pharmaceuticals',
+                'pharmaceutical',
+                'ethnobotany',
+                'ethnopharmacology'
+                'drug',
+                'drugs',
+                'herbals',
+                'herbal',
+                'remedies',
+                'remedy',
+                'homeopathic',
+                'homeopathy',
+                'homeopath',
+                'homeopaths',
+                'immunoglobulin',
+                'immune',
+                'immunoserum'
+                'oil',
+                'oily',
+                'oils',
+                'candles',
+                'candle',
+                'candlenut',
+                'food',
+                'foods',
+                'foodstuff',
+                'foodstuffs',
+                'food-based',
+                'additive',
+                'additives',
+                'edible',
+                'inedible',
+                'beverages',
+                'beverage',
+                'drink',
+                'drinks',
+                'drinking',
+                'tea',
+                'alcohol',
+                'alcoholic',
+                'hydroalcoholic',
+                'cosmetics',
+                'cosmetic',
+                'shampoo',
+                'shampoos',
+                'shampooing',
+                'toxic',
+                'toxicity',
+                'toxicological',
+                'endotoxin',
+                'toxins',
+                'toxin',
+                'endotoxins',
+                'supplement',
+                'supplements',
+                'supplementary',
+                'antioxidants',
+                'antioxidant',
+                'nutraceuticals',
+                'nutraceutical']
+
+# _misc_paired_keywords = []
+# plant_terms = genus_names + family_names
+
+query_name = '_'.join(_en_keywords)  # + _misc_paired_keywords)
+
+
+def get_varied_form_of_word(given_word: str) -> List:
+    # use nltk to add word variations
+    from nltk.corpus import wordnet as wn
+
+    forms = []  # We'll store the derivational forms in a set to eliminate duplicates
+    for happy_lemma in wn.lemmas(given_word):  # for each "happy" lemma in WordNet
+        forms.append(happy_lemma.name())  # add the lemma itself
+        for related_lemma in happy_lemma.derivationally_related_forms():  # for each related lemma
+            forms.append(related_lemma.name())  # add the related lemma
+    if given_word not in forms:
+        forms.append(given_word)
+    return forms
+
+
+def get_varied_forms() -> set:
+    forms = []
+    for w in _en_keywords:
+        forms += get_varied_form_of_word(w)
+
+    return set(forms)
+
+
+varied_keywords = list(get_varied_forms())
+_all_keywords = varied_keywords + genus_names + family_names
+print(f'all variations of keywords: {varied_keywords}')
+
 
 def is_relevant_text(given_text: str) -> str:
     # Note order of this can improve optimisation
     words = [w.strip(string.punctuation) for w in given_text.split()]
 
-    first_match = next((string for string in misc_keywords if string in words), None)
-    if first_match is None:
-        adjacent_words = [" ".join([words[i], words[i + 1]])
-                          for i in range(len(words) - 1)]
-        first_match = next((string for string in misc_paired_keywords if string in adjacent_words), None)
+    first_match = next((string for string in _all_keywords if string in words), None)
+    # if first_match is None:
+    #     adjacent_words = [" ".join([words[i], words[i + 1]])
+    #                       for i in range(len(words) - 1)]
+    #     first_match = next((string for string in _misc_paired_keywords if string in adjacent_words), None)
     return first_match
