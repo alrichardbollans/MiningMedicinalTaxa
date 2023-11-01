c9148cc01be0c12b106c145c2528f00e180cf59c
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-11-01 15:45:32 +0000

    Improve cleaning of title strings. Add methods to save and load filtered texts.

--- literature_downloads/core/get_relevant_papers_from_download.py
+++ literature_downloads/core/get_relevant_papers_from_download.py
@@ -4,9 +4,9 @@ import sys
 
 import pandas as pd
 from tqdm import tqdm
+from typing import List, Union
 
 sys.path.append('../..')
-from literature_downloads import query_name, number_of_keywords, sort_final_dataframe, build_output_dict
 
 scratch_path = os.environ.get('SCRATCH')
 
@@ -24,10 +24,47 @@ for p in [core_download_path, core_text_path, core_paper_info_path, core_abstrac
         os.mkdir(p)
 
 
-def get_relevant_papers_from_download(write_texts=False):
+def clean_title_strings(given_title: str):
+    ## Also need to fix encodings e.g. \\u27
+    fixed_whitespace = ' '.join(given_title.split())
+    return fixed_whitespace
+
+
+def get_info_from_core_paper(paper: dict):
+    corpusid = paper['coreId']
+    try:
+        language = paper['language']['code']
+    except TypeError:
+        language = None
+
+    if len(paper['journals']) >= 1:
+        journals = str(paper['journals'])
+    else:
+        journals = None
+    if len(paper['subjects']) >= 1:
+        subjects = str(paper['subjects'])
+    else:
+        subjects = None
+    if len(paper['topics']) >= 1:
+        topics = str(paper['topics'])
+    else:
+        topics = None
+
+    year = paper['year']
+    issn = paper['issn']
+    doi = paper['doi']
+    title = clean_title_strings(paper['title'])
+    authors = paper['authors']
+    url = paper['downloadUrl']
+
+    return corpusid, language, journals, subjects, topics, year, issn, doi, title, authors, url
+
+
+def get_relevant_papers_from_download():
     paper_df = pd.DataFrame()
 
     import tarfile
+    from literature_downloads import query_name, number_of_keywords, sort_final_dataframe, build_output_dict
 
     zipfile = 'core_2022-03-11_dataset.tar.xz'
     print('unzipping')
@@ -62,41 +99,11 @@ def get_relevant_papers_from_download(write_texts=False):
                             k_word_counts = number_of_keywords(text)
                             if any(len(k_word_counts[kword_type].keys()) > 0 for kword_type in k_word_counts):
                                 paper_count += 1
-                                corpusid = paper['coreId']
-                                if write_texts:
-                                    f = open(os.path.join(core_text_path, corpusid + '.txt'), 'w')
-                                    f.write(text)
-                                    f.close()
-
-                                    if paper['abstract'] is not None:
-                                        f = open(os.path.join(core_abstracts_path, corpusid + '.txt'), 'w')
-                                        f.write(paper['abstract'])
-                                        f.close()
-
-                                try:
-                                    language = paper['language']['code']
-                                except TypeError:
-                                    language = None
-
-                                if len(paper['journals']) >= 1:
-                                    journals = str(paper['journals'])
-                                else:
-                                    journals = None
-                                if len(paper['subjects']) >= 1:
-                                    subjects = str(paper['subjects'])
-                                else:
-                                    subjects = None
-                                if len(paper['topics']) >= 1:
-                                    topics = str(paper['topics'])
-                                else:
-                                    topics = None
-
-                                year = paper['year']
-                                issn = paper['issn']
+                                corpusid, language, journals, subjects, topics, year, issn, doi, title, authors, url = get_info_from_core_paper(paper)
 
                                 info_df = pd.DataFrame(
-                                    build_output_dict(corpusid, paper['doi'], year, k_word_counts, paper['title'], paper['authors'],
-                                                      paper['downloadUrl'], _rel_abstract_path, _rel_text_path, language=language, journals=journals,
+                                    build_output_dict(corpusid, doi, year, k_word_counts, title, authors,
+                                                      url, _rel_abstract_path, _rel_text_path, language=language, journals=journals,
                                                       subjects=subjects, topics=topics, issn=issn))
 
                                 paper_df = pd.concat([paper_df, info_df])
@@ -106,5 +113,70 @@ def get_relevant_papers_from_download(write_texts=False):
     return out_df
 
 
+def save_texts_from_ids(ids: List[int]):
+    ids_to_check = ids + [str(int_id) for int_id in ids]
+    number_of_papers_to_check = len(ids)
+    import tarfile
+
+    zipfile = 'core_2022-03-11_dataset.tar.xz'
+    print('unzipping')
+    with tarfile.open(zipfile, 'r') as main_archive:
+        # This is slow but useful info. # Main archive length: 10251
+        # print(f'Main archive length: {len(main_archive.getnames())}')
+        member_count = 1
+        paper_count = 0
+        for member in main_archive:
+            # iterate over members then get all members out of these
+            print(f'Number {member_count} of main archive containing 10251')
+            member_count += 1
+            print(f'Number of papers collected: {paper_count}')
+            file_obj = main_archive.extractfile(member)
+            with tarfile.open(fileobj=file_obj, mode='r') as sub_archive:
+                # Data providers of each subarchive are here: https://core.ac.uk/data-providers
+                members = sub_archive.getmembers()
+                for i in tqdm(range(len(members))):
+                    m = members[i]
+                    if m.name.endswith('.json'):
+                        f = sub_archive.extractfile(m)
+                        lines = f.readlines()
+                        paper = json.loads(lines[0])
+                        corpusid = paper['coreId']
+                        if corpusid in ids_to_check:
+                            paper_count += 1
+                            text = paper['fullText']
+                            corpusid, language, journals, subjects, topics, year, issn, doi, title, authors, url = get_info_from_core_paper(paper)
+
+                            f = open(os.path.join(core_text_path, corpusid + '.txt'), 'w')
+                            f.write(text)
+                            f.close()
+
+                            if paper['abstract'] is not None:
+                                f = open(os.path.join(core_abstracts_path, corpusid + '.txt'), 'w')
+                                f.write(paper['abstract'])
+                                f.close()
+                            if paper_count == number_of_papers_to_check:
+                                print('All papers saved')
+                                return
+
+
+def load_texts_from_id(given_id: Union[int, str]):
+    text_path = os.path.join(core_text_path, str(given_id) + '.txt')
+    text_file = open(text_path, 'r')
+    # read all lines at once
+    text = text_file.read()
+    # close the file
+    text_file.close()
+
+    abstract_path = os.path.join(core_abstracts_path, str(given_id) + '.txt')
+    abs_file = open(abstract_path, 'r')
+    # read all lines at once
+    abstract = abs_file.read()
+    # close the file
+    abs_file.close()
+    return text, abstract
+
+
 if __name__ == '__main__':
+    # load_texts_from_id(81695610)
+    # save_texts_from_ids([81695610, 41338455])
     get_relevant_papers_from_download()
