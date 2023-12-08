6137bd1ca50932282e9a7959cbb1cce70fddc080
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-12-08 17:15:32 +0000

    Simplify outputs

--- literature_downloads/filter_methods.py
+++ literature_downloads/filter_methods.py
@@ -3,6 +3,8 @@ import string
 from collections import Counter
 from typing import List
 
+import numpy as np
+
 from literature_downloads import get_kword_dict
 
 final_en_keyword_dict = get_kword_dict()
@@ -18,16 +20,15 @@ def get_dict_from_res(count_result: Counter, list_to_check: List[str]):
 
 def number_of_keywords(given_text: str):
     out_dict = {}
-    # start_time = time.time()
+
     words = [w.strip(string.punctuation).lower() for w in given_text.split()]
     # Species names could be 3 words long due to hybrid characters
     paired_words = [" ".join([words[i], words[i + 1]]) for i in range(len(words) - 1)]
     trio_words = [" ".join([words[i], words[i + 1], words[i + 2]]) for i in range(len(words) - 2)]
     potential_words = words + paired_words + trio_words
 
-    res = Counter(potential_words)
     for k in final_en_keyword_dict:
-        out_dict[k] = get_dict_from_res(res, final_en_keyword_dict[k])
+        out_dict[k] = set(potential_words).intersection(final_en_keyword_dict[k])
 
     return out_dict
 
@@ -42,8 +43,12 @@ def build_output_dict(corpusid: str, doi: str, year: int, keyword_counts: dict, 
                 'text_path': [os.path.join(_rel_text_path, corpusid + '.txt')]}
 
     for k in keyword_counts:
-        out_dict[k + '_counts'] = str(keyword_counts[k])
-        out_dict[k + '_total'] = sum(keyword_counts[k].values())
-        out_dict[k + '_unique_total'] = len(keyword_counts[k].keys())
+        unique_counts = len(keyword_counts[k])
+        if unique_counts > 0:
+            out_dict[k + '_counts'] = str(list(keyword_counts[k]))
+        else:
+            out_dict[k + '_counts'] = np.nan
+        # out_dict[k + '_total'] = sum(keyword_counts[k].values())
+        out_dict[k + '_unique_total'] = unique_counts
 
     return out_dict
