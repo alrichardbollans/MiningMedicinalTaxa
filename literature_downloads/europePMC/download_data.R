b59ed7162d714dcf43e5470c5df6feb91f0742d4
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-02-12 07:15:38 +0000

    Remove unused files

--- literature_downloads/europePMC/download_data.R
+++
@@ -1,5 +0,0 @@
-# docs https://github.com/ropensci/europepmc
-# https://docs.ropensci.org/europepmc/articles/introducing-europepmc.html
-library(europepmc)
-
-x = europepmc::epmc_search(query = '"malaria" OR "2019nCoV"')
--- literature_downloads/semantic_scholar/__init__.py
+++
@@ -1,2 +0,0 @@
-from .download_data import *
-from .get_relevant_papers_from_download import *
