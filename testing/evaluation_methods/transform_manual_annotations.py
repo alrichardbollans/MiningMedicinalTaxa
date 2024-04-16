e91272bc4ce8098225b523b860759841c78309ba
fci10kg <f.civita@kew.org>
2024-04-16 11:06:16 +0100

    Transform manual annotation json

--- testing/evaluation_methods/transform_manual_annotations.py
+++ testing/evaluation_methods/transform_manual_annotations.py
@@ -86,11 +86,20 @@ def save_transformed_annotation(data, filename):
 
 def main():
     # Load JSON data from file
-    data_filename = '../test_medicinal_01/manual_annotation/task_for_labelstudio_4187756.json'  # Update with your actual filename
+   # data_filename = '../test_medicinal_01/manual_annotation/task_for_labelstudio_4187756.json'  # Update with your actual filename
+    data_filename = '../test_medicinal_01/manual_annotation/task_for_labelstudio_80818116_ifra_FC_09_04_2024.json'  # Update with your actual filename
     data = load_json_data(data_filename)
 
     # Specify the task ID dynamically (could be user input or another source)
-    task_id_input = 272  # Example task ID
+    #task_id_input = 272  # Example task ID
+   # task_id_input = 282 # Example task ID
+
+   task_id_to_output = {
+        282: 'task_for_labelstudio_80818116_chunk_111.json',
+        283: 'task_for_labelstudio_80818116_chunk_112.json',
+        # Add more mappings as needed
+    }
+
 
     # Transform the data for the specified task ID
     transformed_result = transform_annotation_for_task_id(data, task_id_input)
@@ -102,13 +111,17 @@ def main():
         print(transformed_result)
 
     if transformed_result:
-        output_filename = '../test_medicinal_01/manual_annotation_transformed/task_for_labelstudio_4187756_chunk_0.json'
+        #output_filename = '../test_medicinal_01/manual_annotation_transformed/task_for_labelstudio_4187756_chunk_0.json'
+        output_filename = '../test_medicinal_01/manual_annotation_transformed/task_for_labelstudio_80818116_chunk_111.json'
         save_transformed_annotation(transformed_result, output_filename)
 
 
-    ner_annotations, re_annotations = read_annotation_json('../test_medicinal_01/tasks_completed', '4187756', '0')
-    gt_ner_annotations, gt_re_annotations = read_annotation_json('../test_medicinal_01/manual_annotation_transformed', '4187756', '0')
+    #ner_annotations, re_annotations = read_annotation_json('../test_medicinal_01/tasks_completed', '4187756', '0')
+    #gt_ner_annotations, gt_re_annotations = read_annotation_json('../test_medicinal_01/manual_annotation_transformed', '4187756', '0')
 
+    ner_annotations, re_annotations = read_annotation_json('../test_medicinal_01/tasks_completed', '80818116', '111')
+    gt_ner_annotations, gt_re_annotations = read_annotation_json('../test_medicinal_01/manual_annotation_transformed',
+                                                                 '80818116', '111')
     # Precise match
     precision, recall, f1_score = NER_evaluation(ner_annotations, gt_ner_annotations, precise_NER_annotation_match)
     print(f"All Entities - Precision: {precision}, Recall: {recall}, F1 Score: {f1_score}")
@@ -118,7 +131,7 @@ def main():
     print(f"Scientific Plant Name - Precision: {precision}, Recall: {recall}, F1 Score: {f1_score}")
 
     precision, recall, f1_score = NER_evaluation(ner_annotations, gt_ner_annotations, precise_NER_annotation_match,
-                                               'Medical Condition')
+                                              'Medical Condition')
     print(f"Medical Condition - Precision: {precision}, Recall: {recall}, F1 Score: {f1_score}")
 
     # Approximate match
@@ -127,11 +140,11 @@ def main():
     print(f"All Entities - Approximate Precision: {precision}, Approximate Recall: {recall}, Approximate F1 Score: {f1_score}")
 
     precision, recall, f1_score = NER_evaluation(ner_annotations, gt_ner_annotations, approximate_NER_annotation_match,
-                                                 'Scientific Plant Name')
+                                              'Scientific Plant Name')
     print(f"Scientific Plant Name - Approximate Precision: {precision}, Approximate Recall: {recall}, Approximate F1 Score: {f1_score}")
 
     precision, recall, f1_score = NER_evaluation(ner_annotations, gt_ner_annotations, approximate_NER_annotation_match,
-                                                 'Medical Condition')
+                                               'Medical Condition')
     print(f"Medical Condition - Approximate Precision: {precision}, Approximate Recall: {recall}, Approximate F1 Score: {f1_score}")
 
 
