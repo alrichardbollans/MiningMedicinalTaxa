130840bda3768438a17eec7f0af69e8ea67f1088
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2024-08-21 10:28:16 +0100

    Add methods to clean model outputs by removing non-scientific names

---
+++ rag_models/evaluating/unittestss/removing_names.py
@@ -0,0 +1,32 @@
+# Importing required modules
+import unittest
+
+from rag_models.evaluating.evaluation_methods import TaxaData
+from rag_models.evaluating.evaluation_methods import clean_model_annotations_using_taxonomy_knowledge
+from rag_models.structured_output_schema import Taxon
+
+
+class TestEvaluationMethods(unittest.TestCase):
+
+    def setUp(self):
+        # Create some TaxaData for testing
+        self.model_annotations1 = TaxaData(taxa=[{'scientific_name': 'lion'}, {'scientific_name': 'tiger'}, {'scientific_name': 'bear'}])
+        self.model_annotations2 = TaxaData(taxa=[{'scientific_name': 'cinchona calisaya', 'medical_conditions':['burning', 'stings']}, {'scientific_name': 'rat'}, {'scientific_name': 'lion'}])
+        self.model_annotations3 = TaxaData(taxa=[])
+
+    def test_clean_model_annotations_using_taxonomy_knowledge(self):
+        # Test with model_annotations1
+        result = clean_model_annotations_using_taxonomy_knowledge(self.model_annotations1)
+        self.assertEqual(result.taxa, [])
+
+        # Test with model_annotations2
+        result = clean_model_annotations_using_taxonomy_knowledge(self.model_annotations2)
+        self.assertEqual(result.taxa, [Taxon(scientific_name='cinchona calisaya', medical_conditions=['burning', 'stings'], medicinal_effects=None)])
+
+        # Test with model_annotations3
+        result = clean_model_annotations_using_taxonomy_knowledge(self.model_annotations3)
+        self.assertEqual(result.taxa, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
