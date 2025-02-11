8a5ee150f6448bcb3fbe0a9d8ccea6a898a467af
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2025-02-11 15:42:15 +0000

    Tidy mpns analysis outputs and add found fungi

---
+++ LLM_models/analysis/analyse_fungi_not_in_mpns.py
@@ -0,0 +1,65 @@
+import os
+import string
+
+from LLM_models.analysis.analyse_instances_not_in_MPNS import get_tp_fn_from_annotated_test_data
+
+
+def make_clean_binomial(given_string):
+    # Clean names for a basic match of binomials
+    # Reduce names to two words
+    # lowercase words
+    # remove punctuation
+    out = ' '.join(given_string.split(' ')[:2]).lower()  # first 2 words
+
+    out = out.translate(str.maketrans('', '', string.punctuation))
+
+    return out
+
+
+## Resolve to species
+def resolve_list_to_clean_fungi_df(name_list):
+    # Check no duplicate underscores as these are used to seprate names and conditions.
+    issues = [print(x) for x in name_list if x.count('_') > 1]
+    assert len(issues) == 0
+
+    names_with_medcond = list(set([c.split('_')[0] for c in name_list]))
+    clean_names_with_medcond = [make_clean_binomial(name) for name in names_with_medcond]
+
+    overlaps = set(clean_known_fungi_names).intersection(set(clean_names_with_medcond))
+    return overlaps
+
+
+def your_function():
+    true_positives, false_negatives = get_tp_fn_from_annotated_test_data()
+
+    tp_fungi = resolve_list_to_clean_fungi_df(true_positives)
+
+    with open(os.path.join('outputs', 'mpns_analysis', 'tp_fungi.csv'),
+              'w') as f:
+        for line in tp_fungi:
+            f.write(f"{line}\n")
+
+    fn_fungi = resolve_list_to_clean_fungi_df(false_negatives)
+
+    with open(os.path.join('outputs', 'mpns_analysis', 'fn_fungi.csv'),
+              'w') as f:
+        for line in fn_fungi:
+            f.write(f"{line}\n")
+
+    all_fungi = resolve_list_to_clean_fungi_df(true_positives + false_negatives)
+
+    with open(os.path.join('outputs', 'mpns_analysis', 'all_fungi_in_data.csv'),
+              'w') as f:
+        for line in all_fungi:
+            f.write(f"{line}\n")
+
+
+if __name__ == '__main__':
+    with open(os.path.join('..', '..', 'literature_downloads', 'final_keywords_lists', 'fungi_species_binomials_keywords.txt'), 'r') as f:
+        fungi_species_binomials = f.read().splitlines()
+    ## some minor issues in the species binomials from index fungorum
+    issue_lengths = [x for x in fungi_species_binomials if len(x.split(' ')) in [1, 3, 4]]
+    assert len(issue_lengths) == 6
+    clean_known_fungi_names = [make_clean_binomial(name) for name in fungi_species_binomials]
+
+    your_function()
--- LLM_models/analysis/analyse_instances_not_in_MPNS.py
+++ LLM_models/analysis/analyse_instances_not_in_MPNS.py
@@ -12,36 +12,62 @@ def get_mpns_df():
     acc_name_df.to_csv(os.path.join('inputs', 'MPNS_v12_acc_sp_names.csv'))
     return acc_name_df[['accepted_species']]
 
-
-def main():
-    test = pd.read_csv(os.path.join('..','evaluating','outputs', 'for_testing.csv'))
-    fileNames = os.listdir(os.path.join('..','evaluating','outputs', 'model_errors'))
-    all_names = []
+def get_tp_fn_from_annotated_test_data():
+    test = pd.read_csv(os.path.join('..', 'evaluating', 'outputs', 'for_testing.csv'))
+    fileNames = os.listdir(os.path.join('..', 'evaluating', 'outputs', 'model_errors'))
+    true_positives = []
+    false_negatives = []
     for f in fileNames:
         if f.endswith("ft_gpt-4o-2024-08-06_personal__Acwijdma_problems.csv"):
             if any(f.startswith(f'{str(t)}_') for t in test['id'].unique().tolist()):
-                model_results = pd.read_csv(os.path.join('..','evaluating','outputs', 'model_errors', f), index_col=0)
-                all_names += model_results['MedCond_tp'].dropna().tolist()
-    all_names = list(set(all_names))
+                model_results = pd.read_csv(os.path.join('..', 'evaluating', 'outputs', 'model_errors', f), index_col=0)
+                true_positives += model_results['MedCond_tp'].dropna().tolist()
+                false_negatives += model_results['MedCond_fn'].dropna().tolist()
+    print(f"True positives: {len(true_positives)}")
+    print(f"False negatives: {len(false_negatives)}")
 
-    # Check no duplicate underscores as these are used to seprate names and conditions.
-    issues = [print(x) for x in all_names if x.count('_') > 1]
-    assert len(issues) == 0
+    true_positives = list(set(true_positives))
+    false_negatives = list(set(false_negatives))
+    return true_positives, false_negatives
+def main():
+    true_positives, false_negatives = get_tp_fn_from_annotated_test_data()
+
+
     ## Resolve to species
-    names_with_medcond = list(set([c.split('_')[0] for c in all_names]))
-    name_df = pd.DataFrame(names_with_medcond, columns=['name'])
-    acc_name_df = get_accepted_info_from_names_in_column(name_df, 'name', wcvp_version=_WCVP_VERSION)
-    acc_name_df = acc_name_df[~acc_name_df['accepted_species'].isna()]
-    acc_name_df = acc_name_df.drop_duplicates(subset=['accepted_species'])
-    acc_name_df.to_csv(os.path.join('outputs', 'mpns_analysis', 'accepted_species_with_medCond.csv'))
-    acc_name_df.describe(include='all').to_csv(os.path.join('outputs', 'mpns_analysis', 'accepted_species_with_medCond_summary.csv'))
+    def resolve_list_to_clean_df(name_list):
+        # Check no duplicate underscores as these are used to seprate names and conditions.
+        issues = [print(x) for x in name_list if x.count('_') > 1]
+        assert len(issues) == 0
 
-    plot_native_number_accepted_taxa_in_regions(acc_name_df, 'accepted_species', os.path.join('outputs', 'mpns_analysis'),
+        names_with_medcond = list(set([c.split('_')[0] for c in name_list]))
+        name_df = pd.DataFrame(names_with_medcond, columns=['name'])
+        acc_name_df = get_accepted_info_from_names_in_column(name_df, 'name', wcvp_version=_WCVP_VERSION)
+        acc_name_df = acc_name_df[~acc_name_df['accepted_species'].isna()]
+        acc_name_df = acc_name_df.drop_duplicates(subset=['accepted_species'])
+        return acc_name_df
+
+    tp_acc_name_df = resolve_list_to_clean_df(true_positives)
+    tp_acc_name_df.to_csv(os.path.join('outputs', 'mpns_analysis', 'accepted_species_with_medCond.csv'))
+    tp_acc_name_df.describe(include='all').to_csv(os.path.join('outputs', 'mpns_analysis', 'accepted_species_with_medCond_summary.csv'))
+
+    plot_native_number_accepted_taxa_in_regions(tp_acc_name_df, 'accepted_species', os.path.join('outputs', 'mpns_analysis'),
                                                 'accepted_species_with_medCond.jpg', wcvp_version=_WCVP_VERSION, colormap='inferno')
+
+    fnmatch_acc_name_df = resolve_list_to_clean_df(false_negatives)
+    fnmatch_acc_name_df.to_csv(os.path.join('outputs', 'mpns_analysis', 'fn_accepted_species_with_medCond.csv'))
+    fnmatch_acc_name_df.describe(include='all').to_csv(os.path.join('outputs', 'mpns_analysis', 'fn_accepted_species_with_medCond_summary.csv'))
+
+    all_acc_name_df = resolve_list_to_clean_df(true_positives+false_negatives)
+    all_acc_name_df.to_csv(os.path.join('outputs', 'mpns_analysis', 'all_accepted_species_with_medCond.csv'))
+    all_acc_name_df.describe(include='all').to_csv(os.path.join('outputs', 'mpns_analysis', 'all_accepted_species_with_medCond_summary.csv'))
+
+    plot_native_number_accepted_taxa_in_regions(all_acc_name_df, 'accepted_species', os.path.join('outputs', 'mpns_analysis'),
+                                                'all_accepted_species_with_medCond.jpg', wcvp_version=_WCVP_VERSION, colormap='inferno')
+
 
     ## Check against MPNS
     mpns_df = pd.read_csv(os.path.join('inputs', 'MPNS_v12_acc_sp_names.csv'))
-    species_not_in_mpns = acc_name_df[~acc_name_df['accepted_species'].isin(mpns_df['accepted_species'].values)]
+    species_not_in_mpns = tp_acc_name_df[~tp_acc_name_df['accepted_species'].isin(mpns_df['accepted_species'].values)]
     species_not_in_mpns.to_csv(os.path.join('outputs', 'mpns_analysis', 'species_not_in_mpns.csv'))
     species_not_in_mpns.describe(include='all').to_csv(os.path.join('outputs', 'mpns_analysis', 'species_not_in_mpns_summary.csv'))
 
