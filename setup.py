71bd5a65ca176feebf09fd967cdc0aaaaf7fe990
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2026-02-23 11:40:06 +0000

    Refactor package structure: add `setup.py`, update dependencies in `requirements.txt`

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MiningMedicinalTaxa',
+    url='https://github.com/alrichardbollans/MiningMedicinalTaxa',
+    author='Adam Richard-Bollans',
+    author_email='38588335+alrichardbollans@users.noreply.github.com',
+    # Needed to actually package something
+    packages=find_packages(),
+
+    install_requires=[
+        'langchain==0.3.22',
+        'langchain-core==0.3.83',
+        'pydantic',
+        'wcvpy >= 1.3.2',
+        'openpyxl'
+    ],
+    # *strongly* suggested for sharing
+    version='1.2',
+    description='Collected packages for downloading corpora and extracting plant names',
+    long_description=open('README.md').read(),
+)
